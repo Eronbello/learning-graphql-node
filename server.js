9a4cde08b88ce3a11c0c5e4f77d9acc6f04fe938
Eronbello <eron.bello@gmail.com>
2019-10-10 19:32:56 -0300

    add resolvers

--- server.js
+++ server.js
@@ -21,3 +21,26 @@ const schema = buildSchema(`
 const providers = {
   users: []
 };
+
+let id = 0;
+
+const resolvers = {
+  user({ id }) {
+    return providers.users.find(item => item.id === Number(id));
+  },
+  users() {
+    return providers.users;
+  },
+  createUser({ name, repo, age }) {
+    const user = {
+      id: id++,
+      name,
+      repo,
+      age
+    };
+
+    providers.users.push(user);
+
+    return user;
+  }
+};
